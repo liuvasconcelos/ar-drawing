fdd5427bb14175bbeec8f285b1aa8f47a31f272d
liuvasconcelos <livia.vasconcelos@stant.com.br>
2019-12-08 15:15:41 -0300

    develop Add initial configuration of project.
    - Add rendering function to locate orientation and position of camera.
    -
    
    Signed-off-by: liuvasconcelos <livia.vasconcelos@stant.com.br>

--- AR Drawing/ViewController.swift
+++ AR Drawing/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,
+                                       ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.showsStatistics = true
+        self.sceneView.delegate = self
+        self.sceneView.session.run(configuration)
     }
 
+    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        print("rendering")
+        
+        guard let pointOfView = sceneView.pointOfView else { return }
+        
+        let transform  = pointOfView.transform
+        //get orientation of the camera
+        let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
+        //get location of the camera
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let currentPositionOfCamera = orientation + location
+        
+        print(orientation.x, orientation.y, orientation.z)
+    }
 
+}
+
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
 }
 
